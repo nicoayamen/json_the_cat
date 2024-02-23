17ce5b8bc55b076efa6ca91554824ea282f07980
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-22 21:48:08 -0700

    add logic in order to print out if breed is not found as opposed to general throw err, if found, breed description will print

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,16 @@ const request = require('request');
 const arg = process.argv.slice(2);
 const breed = arg[0];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  console.log('error: ', error);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
   console.log('Status Code: ', response && response.statusCode);
-  console.log('Body: ', body);
-  console.log(typeof body);
-
+  
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-  console.log("!!!",data[0].description);
+  if (data.length === 0) {
+    console.log("Breed not found!");;
+  } else {
+    console.log(data[0].description);
+  }
+
+
 
 });
